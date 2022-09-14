d004c53d470f4549cd817e509710ae1885e5b689
GRIGORII LEDASCHEV <gl@csgo.com>
2022-09-14 10:31:20 +0300

    01_01_02

--- 01/Quadratic_equation.py
+++ 01/Quadratic_equation.py
@@ -1,23 +1,6 @@
 import math
 
 
-def check_solution(solution_function, coefficients):
-    a, b, c = coefficients
-    d = b * b - 4 * (a * c)
-    solution = solution_function(a, b, c)
-    # D < 0 or (0, 0, 0)
-    if (not math.isclose(d, 0, abs_tol=1.0e-10) and d < 0) or\
-            (math.isclose(a, 0, abs_tol=1.0e-10) and
-             math.isclose(b, 0, abs_tol=1.0e-10) and
-             not math.isclose(c, 0, abs_tol=1.0e-10)):
-        return solution is None
-    # x1 and x2 into equation
-    else:
-        x1, x2 = solution
-        return math.isclose(a * x1 * x1 + b * x1 + c, 0, abs_tol=1.0e-10) and\
-               math.isclose(a * x2 * x2 + b * x2 + c, 0, abs_tol=1.0e-10)
-
-
 def solve_equation(a, b, c, verbose=False):
     if math.isclose(a, 0, abs_tol=1.0e-10):
         if math.isclose(b, 0, abs_tol=1.0e-10):
@@ -34,6 +17,23 @@ def solve_equation(a, b, c, verbose=False):
         d = b * b - 4 * (a * c)
         if math.isclose(d, 0, abs_tol=1.0e-10) or d > 0:
             return (-b - math.sqrt(d)) / (2 * a), (-b + math.sqrt(d)) / (2 * a)
+
+
+def check_solution(solution_function, coefficients):
+    a, b, c = coefficients
+    d = b * b - 4 * (a * c)
+    solution = solution_function(a, b, c)
+    # D < 0 or (0, 0, 0)
+    if (not math.isclose(d, 0, abs_tol=1.0e-10) and d < 0) or\
+            (math.isclose(a, 0, abs_tol=1.0e-10) and
+             math.isclose(b, 0, abs_tol=1.0e-10) and
+             not math.isclose(c, 0, abs_tol=1.0e-10)):
+        return solution is None
+    # x1 and x2 into equation
+    else:
+        x1, x2 = solution
+        return math.isclose(a * x1 * x1 + b * x1 + c, 0, abs_tol=1.0e-10) and\
+               math.isclose(a * x2 * x2 + b * x2 + c, 0, abs_tol=1.0e-10)
 
 
 assert check_solution(solve_equation, (0, 0, 0))
