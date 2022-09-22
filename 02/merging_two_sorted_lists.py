3143407f910f28144f7f63eb708832cb5f20e36c
GRIGORII LEDASCHEV <gl@csgo.com>
2022-09-22 17:10:24 +0300

    exists in both

--- 02/merging_two_sorted_lists.py
+++ 02/merging_two_sorted_lists.py
@@ -1,7 +1,3 @@
-import numpy as np
-import random
-
-
 def merge(array_1, array_2):
     """Merging two sorted arrays. Delete duplicates. Each element in result should exist in every array"""
     length_1 = len(array_1)
@@ -30,14 +26,3 @@ assert merge([], ()) == []
 assert merge([1, 1, 2, 5, 7], (1, 1, 2, 3, 4, 7)) == [1, 2, 7]
 assert merge([1, 1, 1, 1, 1], [-1, 1, 1, 1, 1, 1]) == [1]
 assert merge([1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1]) == [1]
-
-
-for _ in range(100):
-    length1 = random.randint(0, 1000)
-    length2 = random.randint(0, 1000)
-    test_array_1 = np.random.randint(50, size=length1)
-    test_array_2 = np.random.randint(50, size=length2)
-    test_array_1.sort()
-    test_array_2.sort()
-    assert merge(test_array_1, test_array_2) == sorted(list(set(test_array_1).intersection(set(test_array_2))))
-print('OK')
