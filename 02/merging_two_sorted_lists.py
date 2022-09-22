1959fc7d09be25f808c6cf337590ee8ffdfa29db
GRIGORII LEDASCHEV <gl@csgo.com>
2022-09-22 17:06:03 +0300

    exist in both

--- 02/merging_two_sorted_lists.py
+++ 02/merging_two_sorted_lists.py
@@ -1,25 +1,43 @@
+import numpy as np
+import random
+
+
 def merge(array_1, array_2):
-    """Merging two sorted arrays"""
+    """Merging two sorted arrays. Delete duplicates. Each element in result should exist in every array"""
     length_1 = len(array_1)
     length_2 = len(array_2)
     merged = []
+    max_appended = 0
     i, j = 0, 0
-    while i < length_1 or j < length_2:
-        if i == length_1 or (j < length_2 and array_1[i] >= array_2[j]):
-            if not merged or (merged and merged[-1] != array_2[j]):
-                merged.append(array_2[j])
+    while i < length_1 and j < length_2:
+        if array_1[i] == array_2[j]:
+            if not merged or array_1[i] != max_appended:
+                merged.append(array_1[i])
+                max_appended = array_1[i]
+            i += 1
+            j += 1
+        elif array_2[j] < array_1[i]:
             j += 1
         else:
-            if not merged or (merged[-1] != array_1[i]):
-                merged.append(array_1[i])
             i += 1
+
     return merged
 
 
-assert merge([1, 1, 2, 5, 7], (1, 1, 2, 3, 4, 7)) == [1, 2, 3, 4, 5, 7]
-assert merge([], (1, 1, 2, 3, 4, 7)) == [1, 2, 3, 4, 7]
-assert merge([1, 1, 2, 5, 7], []) == [1, 2, 5, 7]
-assert merge([1, 1, 1, 1, 1], [-1, 1, 1, 1, 1, 1]) == [-1, 1]
-assert merge([1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1]) == [1]
+assert merge([], (1, 1, 2, 3, 4, 7)) == []
+assert merge([1, 1, 2, 5, 7], []) == []
 assert merge([], ()) == []
+assert merge([1, 1, 2, 5, 7], (1, 1, 2, 3, 4, 7)) == [1, 2, 7]
+assert merge([1, 1, 1, 1, 1], [-1, 1, 1, 1, 1, 1]) == [1]
+assert merge([1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1]) == [1]
+
+
+for _ in range(100):
+    length1 = random.randint(0, 1000)
+    length2 = random.randint(0, 1000)
+    test_array_1 = np.random.randint(50, size=length1)
+    test_array_2 = np.random.randint(50, size=length2)
+    test_array_1.sort()
+    test_array_2.sort()
+    assert merge(test_array_1, test_array_2) == sorted(list(set(test_array_1).intersection(set(test_array_2))))
 print('OK')
