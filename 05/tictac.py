8fdb38a5d882d6804883d53d2907814015caa0e9
Gregor <grigor.ledashev@yandex.ru>
2022-10-07 02:47:01 +0300

    tictac

---
+++ 05/tictac.py
@@ -0,0 +1,99 @@
+class Player:
+    def __init__(self):
+        self.row = [0, 0, 0]
+        self.col = [0, 0, 0]
+        self.diagonal = 0
+        self.semi_diagonal = 0
+
+    def won(self):
+        return max(self.row) == 3 or \
+               max(self.col) == 3 or \
+               self.diagonal == 3 or \
+               self.semi_diagonal == 3
+
+    def update(self, new_row, new_col):
+        self.row[new_col] += 1
+        self.col[new_row] += 1
+        if (new_row, new_col) in ((0, 0), (1, 1), (2, 2)):
+            self.diagonal += 1
+        if (new_row, new_col) in ((0, 2), (1, 1), (2, 0)):
+            self.diagonal += 1
+
+
+class TicTac:
+    def __init__(self):
+        self.step = 0
+        self.matrix = [[-1 for _ in range(3)] for _ in range(3)]
+        self.player_x = Player()
+        self.player_0 = Player()
+
+    @staticmethod
+    def hello():
+        print(
+            '''
+    Assume board looks like:
+        123
+        456
+        789
+    input number to draw a symbol. 
+        ...
+        ...
+        ...
+        '''
+        )
+
+    def input_analysis(self, users_input):
+        result = {'is_valid': False}
+        if len(users_input) != 2:
+            result['message'] = '2 coordinates expected. '
+        elif not all([x.isdigit() for x in users_input]):
+            result['message'] = 'Digits expected. '
+        else:
+            row, col = map(int, users_input)
+            if not 0 <= row <= 2 or not 0 <= col <= 2:
+                result['message'] = 'Numbers between 0 and 2 expected. '
+            elif self.matrix[row][col] != -1:
+                result['message'] = 'Cell is filled. '
+            else:
+                result['is_valid'] = True
+                result['content'] = (row, col)
+        if 'message' in result:
+            result['message'] += 'Try again.'
+        return result
+
+    def show_board(self):
+        for i in range(3):
+            for j in range(3):
+                if self.matrix[i][j] == 0:
+                    print('0', end='')
+                elif self.matrix[i][j] == 1:
+                    print('X', end='')
+                else:
+                    print('-', end='')
+            print()
+
+    def start_game(self):
+        self.hello()
+        while self.step < 9:
+            users_input = input().split()
+            analysis_result = self.input_analysis(users_input)
+            if analysis_result['is_valid']:
+                row, col = analysis_result['content']
+                self.matrix[row][col] = 1
+                self.show_board()
+                if self.player_x.won:
+                    return 'X won'
+                elif self.player_0.won:
+                    return '0 won'
+            else:
+                print(analysis_result['message'])
+        else:
+            return 'Draw'
+
+    def check_winner(self):
+        pass
+
+
+if __name__ == "__main__":
+    game = TicTac()
+    print(game.start_game())
