8b7ec2ae225bbff2e465cba7487b0194a76b1d7c
gregor256 <gl@csgo.com>
2022-11-13 18:34:40 +0100

    adv_2_finish

--- advance_02/server.py
+++ advance_02/server.py
@@ -8,113 +8,115 @@ import requests
 from bs4 import BeautifulSoup
 import click
 
-JOBS_FINISHED = 0
-
-
-def beautifulsoup_extract_text_fallback(response_content):
-    """Delete html tags"""
-    if isinstance(response_content, str):
-        soup = BeautifulSoup(response_content, 'html.parser')
-    else:
-        soup = BeautifulSoup(response_content.text, 'html.parser')
-    text = soup.find_all(text=True)
-
-    cleaned_text = ''
-    blacklist = [
-        '[document]',
-        'noscript',
-        'header',
-        'html',
-        'meta',
-        'head',
-        'input',
-        'script',
-        'style', ]
-
-    for item in text:
-        if item.parent.name not in blacklist:
-            cleaned_text += f'{item} '
-
-    cleaned_text = cleaned_text.replace('\t', '')
-    return cleaned_text.strip()
-
-
-def get_most_common_word(url, top_k_frequent):
-    """get_most_common_word"""
-    # get text
-    request = requests.get(url, timeout=60)
-    # delete tags and make all words lowercase
-    clean_text = beautifulsoup_extract_text_fallback(request).lower()
-    # keep only letters and spaces
-    letters_text = re.sub(r'[^a-zA-Zа-яА-Я ]', '', clean_text)
-    # make list from text
-    words_list = letters_text.split()
-    # delete all one-symbol words
-    words_list = list(filter(lambda word: len(word) > 2, words_list))
-    # count most common words
-    result = dict(Counter(words_list).most_common(top_k_frequent))
-    return f"{url}: {result}"
-
-
-HOST = "127.0.0.1"
-PORT = 5004
-GARBAGE = ['the', 'of', 'and', 'in']
-
-
-def communicate(connections_queue, top_k_frequent, thread_lock):
-    """communicate"""
-    global JOBS_FINISHED
-    while True:
-        try:
-            current_connection = connections_queue.get()
-        except queue.Empty:
-            continue
-
-        current_url = current_connection.recv(1024)
-
-        # If client sent death_pill_url to this thread
-        if current_url.decode() == 'death_pill_url':
-            current_connection.sendall('All urls are processed'.encode())
-            current_connection.close()
-            break
-
-        data_str = get_most_common_word(current_url.decode(), top_k_frequent)
-        current_connection.sendall(data_str.encode())
-        current_connection.close()
-
-        thread_lock.acquire()
-        temp_jobs_finished = JOBS_FINISHED
-        temp_jobs_finished += 1
-        print(f'Processed urls total amount: {temp_jobs_finished}')
-        JOBS_FINISHED = temp_jobs_finished
-        thread_lock.release()
-
 
 @click.command(name="multithread_server")
 @click.argument('n_threads')
-@click.argument('top_k_frequent', type=int)
+@click.argument('top_k_frequent')
 def multithread_server(n_threads, top_k_frequent):
-    """multithread_server"""
-    program_lock = threading.Lock()
-    top_k_most_frequent = int(top_k_frequent)
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as active_socket:
-        active_socket.bind((HOST, PORT))
-        active_socket.listen()
+    """Multithread server"""
 
-        active_queue = queue.Queue()
-        client_threads = [
-            threading.Thread(
-                target=communicate,
-                args=(active_queue, top_k_most_frequent, program_lock)
-            )
-            for _ in range(int(n_threads))
-        ]
+    server = MyServer()
+    server.start_multithread_server(n_threads, top_k_frequent)
 
-        for thread in client_threads:
-            thread.start()
+
+class MyServer:
+    """Server"""
+    def __init__(self):
+        self.jobs_finished = 0
+        self.host = "127.0.0.1"
+        self.port = 5004
+
+    @staticmethod
+    def beautifulsoup_extract_text_fallback(response_content):
+        """Delete html tags"""
+        if isinstance(response_content, str):
+            soup = BeautifulSoup(response_content, 'html.parser')
+        else:
+            soup = BeautifulSoup(response_content.text, 'html.parser')
+        text = soup.find_all(text=True)
+
+        cleaned_text = ''
+        blacklist = [
+            '[document]',
+            'noscript',
+            'header',
+            'html',
+            'meta',
+            'head',
+            'input',
+            'script',
+            'style', ]
+
+        for item in text:
+            if item.parent.name not in blacklist:
+                cleaned_text += f'{item} '
+
+        cleaned_text = cleaned_text.replace('\t', '')
+        return cleaned_text.strip()
+
+    def get_most_common_word(self, url, top_k_frequent):
+        """get_most_common_word"""
+        # get text
+        request = requests.get(url, timeout=60)
+        # delete tags and make all words lowercase
+        clean_text = self.beautifulsoup_extract_text_fallback(request).lower()
+        # keep only letters and spaces
+        letters_text = re.sub(r'[^a-zA-Zа-яА-Я ]', '', clean_text)
+        # make list from text
+        words_list = letters_text.split()
+        # delete all one-symbol words
+        words_list = list(filter(lambda word: len(word) > 2, words_list))
+        # count most common words
+        result = dict(Counter(words_list).most_common(top_k_frequent))
+        return f"{url}: {result}"
+
+    def communicate(self, connections_queue, top_k_frequent, thread_lock):
+        """communicate"""
         while True:
-            active_connection = active_socket.accept()[0]
-            active_queue.put(active_connection)
+            try:
+                current_connection = connections_queue.get()
+            except queue.Empty:
+                continue
+
+            current_url = current_connection.recv(1024)
+
+            # If client sent death_pill_url to this thread
+            if current_url.decode() == 'death_pill_url':
+                current_connection.sendall('All urls are processed'.encode())
+                current_connection.close()
+                break
+
+            data_str = self.get_most_common_word(current_url.decode(), top_k_frequent)
+            current_connection.sendall(data_str.encode())
+            current_connection.close()
+
+            thread_lock.acquire()
+            self.jobs_finished += 1
+            print(f'Processed urls total amount: {self.jobs_finished}')
+            thread_lock.release()
+
+    def start_multithread_server(self, n_threads, top_k_frequent):
+        """multithread_server"""
+        program_lock = threading.Lock()
+        top_k_most_frequent = int(top_k_frequent)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as active_socket:
+            active_socket.bind((self.host, self.port))
+            active_socket.listen()
+
+            active_queue = queue.Queue()
+            client_threads = [
+                threading.Thread(
+                    target=self.communicate,
+                    args=(active_queue, top_k_most_frequent, program_lock)
+                )
+                for _ in range(int(n_threads))
+            ]
+
+            for thread in client_threads:
+                thread.start()
+            while True:
+                active_connection = active_socket.accept()[0]
+                active_queue.put(active_connection)
 
 
 if __name__ == '__main__':
--- advance_02/test_client_server_app.py
+++ advance_02/test_client_server_app.py
@@ -1,10 +1,11 @@
 from unittest import mock
-from server import get_most_common_word
+from server import MyServer
 
 
 def test_most_common_word():
     with open('simple_http.txt', 'r') as http_example:
         with mock.patch("requests.get") as requests_get:
             requests_get.return_value = http_example.read()
-            result = get_most_common_word('https://www.example.com', 1)
+            server = MyServer()
+            result = server.get_most_common_word('https://www.example.com', 1)
             assert result == "https://www.example.com: {'domain': 4}"
