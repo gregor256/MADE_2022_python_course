3ef02a1b9bd3934a1c7e2103e3132875ce5ec1d2
gregor256 <gl@csgo.com>
2022-12-19 18:11:48 +0100

    adv_5_2

--- advance_05/profile_decorator.py
+++ advance_05/profile_decorator.py
@@ -7,24 +7,31 @@ import re
 
 class FuncWithStat:
     """FuncWithStat"""
+
     def __init__(self, function):
         """FuncWithStat"""
         self.namely_function = function
+        self.profiler = cProfile.Profile()
 
     def __call__(self, *args, **kwargs):
         """FuncWithStat"""
-        return self.namely_function(*args, **kwargs)
+        self.profiler.enable()
+        result = self.namely_function(*args, **kwargs)
+        self.profiler.disable()
+        return result
 
     def print_stat(self):
         """FuncWithStat"""
         string_io = io.StringIO()
         sort_by = 'cumulative'
-        p_stats = pstats.Stats(profiler, stream=string_io).sort_stats(sort_by)
+        p_stats = pstats.Stats(self.profiler, stream=string_io).sort_stats(sort_by)
         p_stats.print_stats()
         funcs = string_io.getvalue().split('\n')
+        print(f'{self.namely_function.__name__} function stats: ')
         for line, function_stats in enumerate(funcs):
             if line == 4 or re.search(rf'\({self.namely_function.__name__}\)', function_stats):
                 print(function_stats)
+        print()
 
 
 def profile_deco(function):
@@ -40,26 +47,25 @@ def add(arg1, arg2):
 
 
 @profile_deco
-def no_add():
-    """no_add"""
-    return 0
-
-
-@profile_deco
 def sub(arg1, arg2):
     """sub"""
     return arg1 - arg2
 
 
+@profile_deco
+def no_add():
+    """no_add"""
+    return 0
+
+
 if __name__ == '__main__':
-    profiler = cProfile.Profile()
-    profiler.enable()
     print(add(1, 2))
     print(add(1, 2))
     print(add(1, 2))
+    print(sub(1, 2))
+    print(sub(1, 2))
     print(no_add())
     print(no_add())
     print(no_add())
     add.print_stat()
-
-    profiler.disable()
+    sub.print_stat()
